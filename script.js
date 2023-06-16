b4ebf77002acb9ac5bf7894caa834703d55dd67a
braintech <120840671+bbrainttech@users.noreply.github.com>
2023-06-16 22:56:25 +0100

    onit

--- script.js
+++ script.js
@@ -15,16 +15,32 @@ on('mousedown', all_ls, function (e, i) {
 }, true)
 
 
-const vgi = q('.rt>span input', true)
+const vgi = q('.rt>span input', true),
+    r1 = q('[title="Frank Aguero"] #st_p span', true),
+    r2 = q('[title="John Smith"] #st_p span', true),
+    r3 = q('[title="Harper seven"] #st_p span', true),
+    r4 = q('[title="Malenia lync"] #st_p span', true),
+    r5 = q('[title="Tommy Gun"] #st_p span', true),
 
-vgi.forEach(v => {
+    r_all = [r1, r2, r3, r4, r5];
+
+const add_on = (lst, val) => {
+    lst.forEach(l => cls(l, 'on', 'r'))
+    for (let i = 0; i <= val; i++)cls(lst[i], 'on', 'a')
+
+}
+
+vgi.forEach((v, ind) => {
+    add_on(r_all[ind], parseInt(v.value))
     on('input', v, () => {
         v.style = `--vg : ${10.05 * v.value}%`
+        add_on(r_all[ind], parseInt(v.value))
     })
 })
 
-const battery_el = q('[data-battery-per]')
 
+
+const battery_el = q('[data-battery-per]')
 
 const show_battery = () => {
     if (navigator.getBattery) {
