aa3b4bdd0c8c2e12f39d608be43293f18a33edbf
braintech <120840671+bbrainttech@users.noreply.github.com>
2023-06-18 04:04:56 +0100

    good

--- script.js
+++ script.js
@@ -27,40 +27,61 @@ const vgi = q('.rt>span input', true),
 const add_on = (lst, val) => {
     lst.forEach(l => cls(l, 'on', 'r'))
     for (let i = 0; i <= val; i++)cls(lst[i], 'on', 'a')
-
 }
 
 vgi.forEach((v, ind) => {
     add_on(r_all[ind], parseInt(v.value))
     on('input', v, () => {
-        v.style = `--vg : ${10.05 * v.value}%`
+        v.style = `--vg : ${v.value * 10}%`
         add_on(r_all[ind], parseInt(v.value))
     })
 })
+const nbf = q('.nbf'),
+    addFriend = q('.sf button'),
+    nm = q('.sf #nm'),
+    img_in = q('#img_in');
 
+on('change', img_in, function () {
+    const file = this.files[0],
+        reader = new FileReader();
+    on('load', reader, () => {
+        on('click', addFriend, e => {
+            e.preventDefault()
+            // nbf.innerHTML += `<div data-aos-duration="300" data-aos="fade-up" data-name="${nm.value}"><img src="${reader.result}">${nm.value}</div>`
+        })
+    })
+    reader.readAsDataURL(file)
+})
 
+const battery_el = q('[data-battery-per]'),
+    bat = q('.batt')
 
-const battery_el = q('[data-battery-per]')
+var Bat = ('getBattery' in navigator) ? navigator.getBattery() : null
 
 const show_battery = () => {
-    if (navigator.getBattery) {
-        navigator.getBattery().then(battery => {
-            let bat_lev = (battery.level * 100).toFixed(0), bat_col = '#08a9f4';
-            const isCharging = battery.charging
-            if (isCharging) { cls(q('.batt'), 'charging', 'a') } else { cls(q('.batt'), 'charging', 'r') }
+    if (Bat != null) {
+        Bat.then(bt => {
+            try {
 
-            bat_col = (bat_lev <= 25) ? 'var(--col2)' :
-                (bat_lev <= 50) ? 'var(--col4)' :
-                    (bat_lev <= 75) ? '#70d1f7' :
-                        '#08a9f4'
+                bat_lev = (bt.level * 100).toFixed(0), bat_col = '#08a9f4';
+                const isCharging = bt.charging;
+
+                if (isCharging) { cls(bat, 'charging', 'a') } else { cls(bat, 'charging', 'r') }
 
 
-            q('body').style.setProperty('--battery_col', bat_col)
-            q('[data-props]').style.setProperty('--battery_lev', bat_lev + '%')
+                bat_col = (bat_lev <= 25) ? 'var(--col2)' :
+                    (bat_lev <= 50) ? 'var(--col4)' :
+                        (bat_lev <= 75) ? '#70d1f7' :
+                            '#08a9f4'
 
-            battery_el.textContent = bat_lev + '%'
+
+                q('body').style.setProperty('--battery_col', bat_col)
+                q('[data-props]').style.setProperty('--battery_lev', bat_lev + '%')
+
+                battery_el.textContent = bat_lev + '%'
+            } catch { battery_el.textContent = "can't read battery status please update your browser"; cls(battery_el, 'bad', 'a') }
         })
-    }
+    } else return
 }
 setInterval(show_battery, 10)
 show_battery()
